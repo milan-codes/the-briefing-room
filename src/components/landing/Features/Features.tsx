4308b6f036308b38db6322842d4bc1c91cd9be09
milan-codes <github@milancodes.io>
2023-02-11 16:00:01 +0100

    Broke features block into smaller components

--- src/components/landing/Features/Features.tsx
+++ src/components/landing/Features/Features.tsx
@@ -1,10 +1,11 @@
-import FeatureBlock, { FeatureBlockProps } from "./FeatureBlock";
+import { BuildingCommunity, Medal, TrendingUp } from "tabler-icons-react";
+import FeatureBlock from "./FeatureBlock";
 
 const Features: React.FC = () => (
-  <section className="bg-blue-900 text-gray-50 min-h-screen">
+  <section className="bg-blue-900 text-gray-50">
     <div className="mx-auto max-w-screen-lg px-4 py-16">
       <div className="mx-auto max-w-3xl text-center">
-        <h2 className="text-3xl font-bold sm:text-4xl">What makes this software special</h2>
+        <h2 className="text-3xl font-bold sm:text-4xl">Built by fans</h2>
 
         <p className="mt-4 text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores iure fugit totam
@@ -14,188 +15,33 @@ const Features: React.FC = () => (
       </div>
 
       <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
-        <div className="flex items-start">
-          <span className="flex-shrink-0 rounded-lg bg-gray-800 p-4">
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
-          </span>
-
-          <div className="ml-4">
-            <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-            <p className="mt-1 text-sm text-gray-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-              possimus quisquam reiciendis tempora animi! Quaerat, saepe?
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-start">
-          <span className="flex-shrink-0 rounded-lg bg-gray-800 p-4">
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
-          </span>
-
-          <div className="ml-4">
-            <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-            <p className="mt-1 text-sm text-gray-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-              possimus quisquam reiciendis tempora animi! Quaerat, saepe?
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-start">
-          <span className="flex-shrink-0 rounded-lg bg-gray-800 p-4">
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
-          </span>
-
-          <div className="ml-4">
-            <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-            <p className="mt-1 text-sm text-gray-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-              possimus quisquam reiciendis tempora animi! Quaerat, saepe?
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-start">
-          <span className="flex-shrink-0 rounded-lg bg-gray-800 p-4">
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
-          </span>
-
-          <div className="ml-4">
-            <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-            <p className="mt-1 text-sm text-gray-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-              possimus quisquam reiciendis tempora animi! Quaerat, saepe?
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-start">
-          <span className="flex-shrink-0 rounded-lg bg-gray-800 p-4">
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
-          </span>
-
-          <div className="ml-4">
-            <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-            <p className="mt-1 text-sm text-gray-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-              possimus quisquam reiciendis tempora animi! Quaerat, saepe?
-            </p>
-          </div>
-        </div>
-
-        <div className="flex items-start">
-          <span className="flex-shrink-0 rounded-lg bg-gray-800 p-4">
-            <svg
-              className="h-5 w-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-              <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              ></path>
-            </svg>
-          </span>
-
-          <div className="ml-4">
-            <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-            <p className="mt-1 text-sm text-gray-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque tempore est ab
-              possimus quisquam reiciendis tempora animi! Quaerat, saepe?
-            </p>
-          </div>
-        </div>
+        {featureItems.map((item) => (
+          <FeatureBlock key={item.title} {...item} />
+        ))}
       </div>
     </div>
   </section>
 );
+
+const featureItems = [
+  {
+    title: "Feature 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores iure fugit totam iste obcaecati.",
+    icon: <BuildingCommunity />,
+  },
+  {
+    title: "Feature 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores iure fugit totam iste obcaecati.",
+    icon: <TrendingUp />,
+  },
+  {
+    title: "Feature 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores iure fugit totam iste obcaecati.",
+    icon: <Medal />,
+  },
+];
 
 export default Features;
