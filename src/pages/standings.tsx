1ef422174ba64e55e35371efe864c6d09f590e2e
milan-codes <milanherke@protonmail.com>
2023-02-12 18:06:27 +0100

    Created a separate Table component to display championship standings

--- src/pages/standings.tsx
+++ src/pages/standings.tsx
@@ -4,6 +4,7 @@ import Navbar from "../components/landing/Navbar";
 import { DriverStanding } from "../model/Standing";
 import getUnicodeFlagIcon from "country-flag-icons/unicode";
 import _ from "lodash";
+import Table from "../components/standings/Table";
 
 export interface StandingsProps {
   standings: DriverStanding[];
@@ -21,111 +22,42 @@ const getCountryFlag = (nationality: string) => {
 };
 
 const Standings: NextPage<StandingsProps> = ({ standings }) => {
-  const constructionStandings = _.groupBy(
+  const constructorStandings = _.groupBy(
     standings,
     (standing) => standing.Constructors[0].constructorId
   );
+
+  const wdcTableData = standings.map((standing, index) => [
+    `${index + 1}`,
+    `${standing.Driver.permanentNumber}`,
+    `${getCountryFlag(standing.Driver.nationality)} ${standing.Driver.givenName} ${
+      standing.Driver.familyName
+    }`,
+    `${standing.Constructors[0].name}`,
+    `${standing.points}`,
+    `${standing.wins}`,
+  ]);
+
+  const wccTableData = Object.keys(constructorStandings).map((key, index) => [
+    `${index + 1}`,
+    `${constructorStandings[key][0].Constructors[0].name}`,
+    `${constructorStandings[key].reduce((acc, curr) => acc + parseInt(curr.points), 0)}`,
+    `${constructorStandings[key].reduce((acc, curr) => acc + parseInt(curr.wins), 0)}`,
+  ]);
+
   return (
     <div className="bg-gray-50 dark:bg-gray-900">
       <Navbar />
-      <div className="mx-auto max-w-screen-lg px-4 py-8 overflow-x-auto">
-        <h1 className="text-3xl font-extrabold mb-8 text-gray-900 dark:text-gray-100">
-          Latest WDC standings
-        </h1>
-        <table className="min-w-full divide-y-2 divide-gray-300 dark:divide-gray-700 text-sm">
-          <thead>
-            <tr>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Position
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Driver #
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Driver
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Constructor
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Points
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Wins
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="min-w-full divide-y-2 divide-gray-300 dark:divide-gray-700 text-sm">
-            {standings.map((standing, index) => (
-              <tr key={standing.Driver.driverId} className="odd:bg-gray-200 dark:odd:bg-gray-800">
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {index + 1}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {standing.Driver.permanentNumber}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {getCountryFlag(standing.Driver.nationality)} {standing.Driver.givenName}{" "}
-                  {standing.Driver.familyName}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {standing.Constructors[0].name}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {standing.points}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {standing.wins}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <div className="mx-auto max-w-screen-lg px-4 py-8 overflow-x-auto">
-        <h1 className="text-3xl font-extrabold mb-8 text-gray-900 dark:text-gray-100">
-          Latest WCC standings
-        </h1>
-        <table className="min-w-full divide-y-2 divide-gray-300 dark:divide-gray-700 text-sm">
-          <thead>
-            <tr>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Position
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Constructor
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Points
-              </th>
-              <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                Wins
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="min-w-full divide-y-2 divide-gray-300 dark:divide-gray-700 text-sm">
-            {Object.keys(constructionStandings).map((key, index) => (
-              <tr key={key} className="odd:bg-gray-200 dark:odd:bg-gray-800">
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {index + 1}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {constructionStandings[key][0].Constructors[0].name}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {constructionStandings[key].reduce((acc, curr) => acc + parseInt(curr.points), 0)}
-                </td>
-                <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
-                  {constructionStandings[key].reduce((acc, curr) => acc + parseInt(curr.wins), 0)}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <Table
+        title="Latest WDC standings"
+        headers={["Position", "Driver #", "Driver", "Team", "Points", "Wins"]}
+        data={wdcTableData}
+      />
+      <Table
+        title="Latest WCC standings"
+        headers={["Position", "Team", "Points", "Wins"]}
+        data={wccTableData}
+      />
       <Footer />
     </div>
   );
