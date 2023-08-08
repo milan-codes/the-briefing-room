488f3befa6e27b661c6880fe3429241ce8535e87
milan-codes <milanherke@protonmail.com>
2023-08-08 12:58:15 +0200

    Session starting times are now displayed in season hub events

--- src/pages/season-hub/[event].tsx
+++ src/pages/season-hub/[event].tsx
@@ -4,6 +4,9 @@ import { GrandPrix, Season } from "../../model/Season";
 import { slugify } from "../../utils/slugify";
 import Navbar from "../../components/landing/Navbar/Navbar";
 import { getCountryFlagByCode } from "../../utils/getCountryFlagCode";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import GrandPrixInfoHeader from "../../components/season-hub/GrandPrixInfoHeader/GrandPrixInfoHeader";
+import Footer from "../../components/landing/Footer/Footer";
 
 interface EventPreviewProps {
   grandprix: GrandPrix;
@@ -12,28 +15,69 @@ interface EventPreviewProps {
 const EventPreview: React.FC<EventPreviewProps> = ({ grandprix }) => {
   const eventType =
     grandprix.EventFormat === "conventional" ? "Conventional weekend" : "Sprint weekend";
+
+  const sessions = [
+    {
+      name: grandprix.Session1,
+      date: new Date(grandprix.Session1Date),
+    },
+    {
+      name: grandprix.Session2,
+      date: new Date(grandprix.Session2Date),
+    },
+    {
+      name: grandprix.Session3,
+      date: new Date(grandprix.Session3Date),
+    },
+    {
+      name: grandprix.Session4,
+      date: new Date(grandprix.Session4Date),
+    },
+    {
+      name: grandprix.Session5,
+      date: new Date(grandprix.Session5Date),
+    },
+  ];
+
   return (
     <div className="bg-gray-50 dark:bg-gray-900">
       <div className="mx-auto max-w-screen-lg">
         <Navbar />
+        <GrandPrixInfoHeader
+          eventName={grandprix.OfficialEventName}
+          location={grandprix.Location}
+          country={grandprix.Country}
+          startDate={grandprix.Session1Date}
+          endDate={grandprix.Session5Date}
+          eventType={eventType}
+        />
         <div className="px-4 mt-8">
-          <h1 className="text-3xl font-extrabold mb-3 text-gray-900 dark:text-gray-100">
-            {grandprix.OfficialEventName}
-          </h1>
-          <p className="text-gray-600 dark:text-gray-400 mb-1">
-            <span className="font-extrabold">Location: </span> {grandprix.Location},{" "}
-            {grandprix.Country} {getCountryFlagByCode(grandprix.Country)}
-          </p>
-          <p className="text-gray-600 dark:text-gray-400 mb-1">
-            <span className="font-extrabold">Date: </span>{" "}
-            {new Date(grandprix.Session1Date).toLocaleDateString()} -{" "}
-            {new Date(grandprix.Session5Date).toLocaleDateString()}
-          </p>
-          <p className="text-gray-600 dark:text-gray-400 mb-1">
-            <span className="font-extrabold">Format: </span>
-            {eventType}
-          </p>
+          <div className="mt-4">
+            <div className="flex justify-between flex-col md:flex-row">
+              {sessions.map((session) => (
+                <div>
+                  <h2 className="text-xl font-bold mb-2 text-gray-900 dark:text-gray-100">
+                    {session.name}
+                  </h2>
+                  <p className="text-gray-600 dark:text-gray-400 mb-1">
+                    {session.date.toLocaleDateString([], {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </p>
+                  <p className="text-gray-600 dark:text-gray-400 mb-1">
+                    {session.date.toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+        <Footer />
       </div>
     </div>
   );
@@ -55,7 +99,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   let event = "";
-  if (params?.event) event = params.event.toString().split("-")[0];
+  if (params?.event) {
+    // replace - with space
+    event = params.event.toString().replace(/-/g, " ");
+  }
   const res = await fetch(
     `${process.env.SERVER}/grandprix?year=${new Date().getFullYear()}&name=${event}`
   );
