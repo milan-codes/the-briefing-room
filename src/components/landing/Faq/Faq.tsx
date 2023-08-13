5816ab5b271275b1cd0b124acf84f4a3f424fdab
milan-codes <milanherke@protonmail.com>
2023-08-13 20:30:13 +0200

    Changed placeholder text on landing page

--- src/components/landing/Faq/Faq.tsx
+++ src/components/landing/Faq/Faq.tsx
@@ -17,29 +17,29 @@ const Faq: React.FC = () => {
 
 const items: FaqItemProps[] = [
   {
+    title: "Is this unofficial software?",
+    description:
+      "Yes, it is UNOFFICIAL. The creator of this website is in no way, shape or form linked to FORMULA 1, Liberty Media, the FIA or any other organization. This website was created only for fun, and educational purposes and it does not generate any revenue. All rights belong to their respective owners. It is a web application developed by passionate fans of the sport to provide fellow enthusiasts with a unique way to analyze and engage with telemetry data and statistics. Any insights or data derived from the app should be regarded as unofficial and for informational purposes only.",
+  },
+  {
     title: "Where do we get the data from?",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "The data is provided by the amazing FastF1 package. Checkout their GitHub page & docs for more information.",
   },
   {
     title: "How accurate is the data?",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "There is a margin of error in telemetry data, attributed to the intermittent nature of the data samples, which prevents the dataset from being entirely continuous in nature.",
   },
   {
-    title: "Do you collect personal information about me?",
+    title: "Do you collect any data about me?",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    title: "Is this unofficial software?",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Absolutely not. Your interactions with the app, and usage remain entirely confidential and are not gathered or stored by us.",
   },
   {
     title: "How can I support the project?",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "We welcome and encourage contributions from the community to enhance The Briefing Room app. If you have coding skills, insights, or ideas to improve the app, you can actively participate in its development on the project's GitHub repositories. Whether it's fixing bugs, adding new features, or suggesting improvements, your input can make a significant difference. By collaborating with us on GitHub, you can help shape the future of this app and create a more comprehensive and valuable resource for all F1 fans.",
   },
 ];
 
