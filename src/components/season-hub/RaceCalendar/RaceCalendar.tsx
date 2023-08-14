a65699580831f78cb569bbe7b52325093588fd2c
milan-codes <milanherke@protonmail.com>
2023-08-14 12:06:36 +0200

    Fixed missing key attributes in season-hub

--- src/components/season-hub/RaceCalendar/RaceCalendar.tsx
+++ src/components/season-hub/RaceCalendar/RaceCalendar.tsx
@@ -13,8 +13,8 @@ const RaceCalendar: React.FC<RaceCalendarProps> = ({ season }) => {
         Season schedule
       </h1>
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 px-4">
-        {season[0].events.map((event) => (
-          <RaceCalendarListItem event={event} />
+        {season[0].events.map((event, index) => (
+          <RaceCalendarListItem key={index} event={event} />
         ))}
       </ul>
     </div>
--- src/components/standings/Table/Table.tsx
+++ src/components/standings/Table/Table.tsx
@@ -10,8 +10,11 @@ const Table: React.FC<TableProps> = ({ title, headers, data }) => (
     <table className="min-w-full divide-y-2 divide-gray-300 dark:divide-gray-700 text-sm">
       <thead>
         <tr>
-          {headers.map((header) => (
-            <th className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
+          {headers.map((header, index) => (
+            <th
+              key={index}
+              className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100"
+            >
               {header}
             </th>
           ))}
@@ -21,8 +24,11 @@ const Table: React.FC<TableProps> = ({ title, headers, data }) => (
       <tbody className="min-w-full divide-y-2 divide-gray-300 dark:divide-gray-700 text-sm">
         {data.map((row, index) => (
           <tr key={index} className="odd:bg-gray-200 dark:odd:bg-gray-800">
-            {row.map((cell) => (
-              <td className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100">
+            {row.map((cell, index) => (
+              <td
+                key={index}
+                className="whitespace-nowrap px-4 py-2 text-left font-medium text-gray-900 dark:text-gray-100"
+              >
                 {cell}
               </td>
             ))}
